2cc836d19fce5a227285744d0af091061b330a08
KpStar <kpstar@KpStars-Mac.local>
2018-02-25 18:06:59 -0800

    Initial Commit

---
+++ Inspection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inspection
+//
+//  Created by KpStar on 2/25/18.
+//  Copyright © 2018 SaudiArabia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
